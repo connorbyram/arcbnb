cf30b5cfd0414c136344aaff57392f41b12efacf
mjmickd <mjmickd@gmail.com>
2023-03-06 16:30:34 -0800

    Monday Afternoon push

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from .models import Listing
+from .forms import BookingForm
 
 
 # Create your views here.
@@ -14,10 +15,19 @@ def listings_index(request):
     })
 def listings_detail(request, listing_id):
    listing = Listing.objects.get(id=listing_id)
+   booking_form = BookingForm()
    return render(request, 'listings/detail.html', {
-      'listing': listing
+      'listing': listing, 'booking_form': booking_form
    })
   
+def add_booking(request, listing_id, user_id):
+   form = BookingForm(request.POST)
+   if form.is_valid():
+      new_booking = form.save(commit=False)
+      new_booking.listing_id = listing_id
+      new_booking.user = user_id 
+      new_booking.save()
+   return redirect('/', listing_id=listing_id)
 
 
 
